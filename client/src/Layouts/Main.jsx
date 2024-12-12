4998dd8e5e71d9bd86859b6d762a7cf391413ec4
NabilaFerdousPrapty <nabilaferdousprapty@gmail.com>
2024-12-12 21:31:29 +0600

    router setup

--- client/src/Layouts/Main.jsx
+++ client/src/Layouts/Main.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import Sidebar from '../Components/SideBar';
+import NoChatSelected from '../Components/NoChatSelected';
 
 const Main = () => {
+    const user = null;
   return (
-    <div>
-        
+    <div className="h-screen bg-slate-300 ">
+      <div className="flex items-center justify-center pt-20 px-4">
+        <div className="bg-base-100 rounded-lg shadow-cl w-full max-w-6xl h-[calc(100vh-8rem)]">
+          <div className="flex h-full rounded-lg overflow-hidden ">
+            <Sidebar />
+
+            {!user ? <NoChatSelected /> : <ChatContainer />}
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,45 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.jsx';
-import { CssBaseline } from '@mui/material'; 
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import AuthProvider from "./Providers/AuthProvider.jsx";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router"; 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import Main from "./Layouts/Main.jsx";
+import SignUpPage from "./Pages/SignUpPage.jsx";
+import LoginPage from "./Pages/LoginPage.jsx";
+import ProfilePage from "./Pages/ProfilePage.jsx";
 
-createRoot(document.getElementById('root')).render(
+const queryClient = new QueryClient();
+const user = null; // Example for testing. Replace with actual user logic.
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <CssBaseline />
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        {/* Wrap Routes in BrowserRouter */}
+        <BrowserRouter>
+          <Routes>
+            <Route
+              path="/"
+              element={user ? <Main /> : <Navigate to="/login" replace />}
+            />
+            <Route
+              path="/signup"
+              element={!user ? <SignUpPage /> : <Navigate to="/" replace />}
+            />
+            <Route
+              path="/login"
+              element={!user ? <LoginPage /> : <Navigate to="/" replace />}
+            />
+            <Route
+              path="/profile"
+              element={
+                user ? <ProfilePage /> : <Navigate to="/login" replace />
+              }
+            />
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
+    </QueryClientProvider>
   </StrictMode>
 );
